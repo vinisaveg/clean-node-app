3a69db08aedf89b9e40b9cc8ac125d0dbd1472e6
vinisaveg <viniisaveeg@gmail.com>
2022-02-27 15:05:42 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+module.exports = {
+  roots: ['<rootDir>/src'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': "<rootDir>/src/$1"
+  }
+};
