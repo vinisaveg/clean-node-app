11ae205888cbc0f6654dfa8f56588e1e347fc304
vinisaveg <viniisaveeg@gmail.com>
2022-03-10 09:38:10 -0300

    refactor: create a makeSut factory helper function

--- src/data/use-cases/sign-up/remote-sign-up.spec.ts
+++ src/data/use-cases/sign-up/remote-sign-up.spec.ts
@@ -1,12 +1,29 @@
+import { SignUpParams } from "@/domain/use-cases/sign-up";
 import { RemoteSignUp } from "./remote-sign-up";
 import { AddUserRepositorySpy } from "./test/add-user-repository-spy";
 import { CheckEmailRepositorySpy } from "./test/check-email-repository-spy";
 
+type SutType = {
+  checkEmailRepositorySpy: CheckEmailRepositorySpy;
+  addUserRepositorySpy: AddUserRepositorySpy;
+  sut: RemoteSignUp;
+};
+
+const makeSut = (): SutType => {
+  const checkEmailRepositorySpy = new CheckEmailRepositorySpy();
+  const addUserRepositorySpy = new AddUserRepositorySpy();
+  const sut = new RemoteSignUp(checkEmailRepositorySpy, addUserRepositorySpy);
+
+  return {
+    checkEmailRepositorySpy,
+    addUserRepositorySpy,
+    sut,
+  };
+};
+
 describe("Remote Sign Up use-case", () => {
   it("Should execute with correct values", async () => {
-    const checkEmailRepositorySpy = new CheckEmailRepositorySpy();
-    const addUserRepositorySpy = new AddUserRepositorySpy();
-    const sut = new RemoteSignUp(checkEmailRepositorySpy, addUserRepositorySpy);
+    const { checkEmailRepositorySpy, addUserRepositorySpy, sut } = makeSut();
 
     const signUpData = {
       name: "name",
@@ -24,9 +41,7 @@ describe("Remote Sign Up use-case", () => {
   });
 
   it("Should call checkEmailRepository with correct e-mail value", async () => {
-    const checkEmailRepositorySpy = new CheckEmailRepositorySpy();
-    const addUserRepositorySpy = new AddUserRepositorySpy();
-    const sut = new RemoteSignUp(checkEmailRepositorySpy, addUserRepositorySpy);
+    const { checkEmailRepositorySpy, sut } = makeSut();
 
     const signUpData = {
       name: "name",
@@ -42,9 +57,7 @@ describe("Remote Sign Up use-case", () => {
   });
 
   it("Should return false if CheckEmailRepository returns true", async () => {
-    const checkEmailRepositorySpy = new CheckEmailRepositorySpy();
-    const addUserRepositorySpy = new AddUserRepositorySpy();
-    const sut = new RemoteSignUp(checkEmailRepositorySpy, addUserRepositorySpy);
+    const { checkEmailRepositorySpy, sut } = makeSut();
 
     const signUpData = {
       name: "name",
@@ -60,9 +73,7 @@ describe("Remote Sign Up use-case", () => {
   });
 
   it("Should return true if CheckEmailRepository returns false", async () => {
-    const checkEmailRepositorySpy = new CheckEmailRepositorySpy();
-    const addUserRepositorySpy = new AddUserRepositorySpy();
-    const sut = new RemoteSignUp(checkEmailRepositorySpy, addUserRepositorySpy);
+    const { checkEmailRepositorySpy, sut } = makeSut();
 
     const signUpData = {
       name: "name",
@@ -78,9 +89,7 @@ describe("Remote Sign Up use-case", () => {
   });
 
   it("Should throw error if CheckEmailRepository throws", async () => {
-    const checkEmailRepositorySpy = new CheckEmailRepositorySpy();
-    const addUserRepositorySpy = new AddUserRepositorySpy();
-    const sut = new RemoteSignUp(checkEmailRepositorySpy, addUserRepositorySpy);
+    const { checkEmailRepositorySpy, sut } = makeSut();
 
     jest.spyOn(checkEmailRepositorySpy, "check").mockImplementationOnce(() => {
       throw new Error();
