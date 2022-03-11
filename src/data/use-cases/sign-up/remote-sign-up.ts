82e908ee05ce6edd405a6ef73b7c4d25ae3e286e
vinisaveg <viniisaveeg@gmail.com>
2022-03-11 09:41:42 -0300

    feat: add hasher to RemoteSignUp

--- src/data/use-cases/sign-up/remote-sign-up.ts
+++ src/data/use-cases/sign-up/remote-sign-up.ts
@@ -1,22 +1,26 @@
 import { SignUp, SignUpParams, SignUpResult } from "@/domain/use-cases/sign-up";
 import { AddUserRepository } from "@/data/protocols/user/add-user-repository";
 import { CheckEmailRepository } from "@/data/protocols/user/check-email-repository";
+import { Hasher } from "@/data/protocols/cryptography/hasher";
 
 export class RemoteSignUp implements SignUp {
   constructor(
     private readonly checkEmailRepository: CheckEmailRepository,
+    private readonly hasher: Hasher,
     private readonly addUserRepository: AddUserRepository
   ) {}
 
   async execute(data: SignUpParams): Promise<SignUpResult> {
     const isEmailTaken = await this.checkEmailRepository.execute(data.email);
 
-    if (isEmailTaken) {
-      return {
-        result: false,
-      };
+    if (!isEmailTaken) {
+      await this.hasher.hash(data.password);
+
+      return this.addUserRepository.execute(data);
     }
 
-    return this.addUserRepository.execute(data);
+    return {
+      result: false,
+    };
   }
 }
