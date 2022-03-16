b64d13096b624cd3f25929fdba3234d53b9bdb68
vinisaveg <viniisaveeg@gmail.com>
2022-03-16 19:00:48 -0300

    feat: add encrypter to generate accessToken

--- src/data/use-cases/sign-up/remote-sign-up.ts
+++ src/data/use-cases/sign-up/remote-sign-up.ts
@@ -2,12 +2,14 @@ import { SignUp, SignUpParams, SignUpResult } from "@/domain/use-cases/sign-up";
 import { AddUserRepository } from "@/data/protocols/user/add-user-repository";
 import { CheckEmailRepository } from "@/data/protocols/user/check-email-repository";
 import { Hasher } from "@/data/protocols/cryptography/hasher";
+import { Encrypter } from "@/data/protocols/cryptography/encrypter";
 
 export class RemoteSignUp implements SignUp {
   constructor(
     private readonly checkEmailRepository: CheckEmailRepository,
     private readonly hasher: Hasher,
-    private readonly addUserRepository: AddUserRepository
+    private readonly addUserRepository: AddUserRepository,
+    private readonly encrypter: Encrypter
   ) {}
 
   async execute(data: SignUpParams): Promise<SignUpResult> {
@@ -22,10 +24,12 @@ export class RemoteSignUp implements SignUp {
         password: hashedPassword,
       });
 
+      const accessToken = await this.encrypter.encrypt(addUserResult.id);
+
       return {
         result: true,
         name: addUserResult.name,
-        accessToken: "token",
+        accessToken,
       };
     }
 
