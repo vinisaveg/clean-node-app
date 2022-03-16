434c3a3262960bec06acc5ef33b0b7e459425942
vinisaveg <viniisaveeg@gmail.com>
2022-03-16 19:43:05 -0300

    chore: add faker to signUpParams

--- src/data/use-cases/sign-up/test/mocks/mock-sign-up.ts
+++ src/data/use-cases/sign-up/test/mocks/mock-sign-up.ts
@@ -1,9 +1,11 @@
 import { SignUpParams } from "@/domain/use-cases/sign-up";
 
+import { faker } from "@faker-js/faker";
+
 export const mockSignUpParams = (): SignUpParams => {
   return {
-    name: "name",
-    email: "name@email.com",
-    password: "1234567890",
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password: faker.random.alphaNumeric(7),
   };
 };
