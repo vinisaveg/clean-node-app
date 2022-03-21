e3a06713d7639d6082b45ed8abe82f3600a69171
vinisaveg <viniisaveeg@gmail.com>
2022-03-21 15:54:44 -0300

    test: ensure to return a valid hashed text

--- src/infra/cryptography/bcrypt/bcrypt-hasher.spec.ts
+++ src/infra/cryptography/bcrypt/bcrypt-hasher.spec.ts
@@ -1,5 +1,11 @@
 import { BcryptHasher } from "./bcrypt-hasher";
 
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return "hashedText";
+  },
+}));
+
 describe("BcryptHasher implementation", () => {
   it("Should call hash with correct text value", async () => {
     const salt = 10;
@@ -11,5 +17,15 @@ describe("BcryptHasher implementation", () => {
     await sut.hash(text);
 
     expect(bcryptHasherSpy).toHaveBeenCalledWith(text);
+  });
+
+  it("Should return a valid hashed text", async () => {
+    const salt = 10;
+    const text = "1234567890";
+    const sut = new BcryptHasher(salt);
+
+    const hashedText = await sut.hash(text);
+
+    expect(hashedText).toBe("hashedText");
   });
 });
