345a71e70c9f53973466f53735f92b881900f931
vinisaveg <viniisaveeg@gmail.com>
2022-03-21 18:53:50 -0300

    test: ensure result is a valid accessToken

--- src/infra/cryptography/jwt/jwt-encrypter.spec.ts
+++ src/infra/cryptography/jwt/jwt-encrypter.spec.ts
@@ -1,6 +1,12 @@
 import faker from "@faker-js/faker";
 import { JwtEncrypter } from "@/infra/cryptography/jwt/jwt-encrypter";
 
+jest.mock("jsonwebtoken", () => ({
+  async sign(): Promise<string> {
+    return "accessToken";
+  },
+}));
+
 describe("JwtEncrypter implementation", () => {
   it("Should call encrypt with correct value", async () => {
     const sut = new JwtEncrypter("secret", "HS256");
@@ -12,5 +18,15 @@ describe("JwtEncrypter implementation", () => {
     await sut.encrypt(text);
 
     expect(jwtEncrypterSpy).toBeCalledWith(text);
+  });
+
+  it("Should return a valid accessToken", async () => {
+    const sut = new JwtEncrypter("secret", "HS256");
+
+    const text = faker.random.alphaNumeric(24);
+
+    const result = await sut.encrypt(text);
+
+    expect(result).toBe("accessToken");
   });
 });
