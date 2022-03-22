7c21bdf2c57b30d99c123e4ca45410613754a0a6
vinisaveg <viniisaveeg@gmail.com>
2022-03-22 17:13:43 -0300

    refactor: add default option params to mockSignUpParams

--- test/mocks/sign-up/mock-sign-up.ts
+++ test/mocks/sign-up/mock-sign-up.ts
@@ -2,10 +2,14 @@ import { SignUpParams } from "@/domain/use-cases/sign-up";
 
 import { faker } from "@faker-js/faker";
 
-export const mockSignUpParams = (): SignUpParams => {
+export const mockSignUpParams = (
+  name = faker.name.findName(),
+  email = faker.internet.email(),
+  password = faker.internet.password()
+): SignUpParams => {
   return {
-    name: faker.name.findName(),
-    email: faker.internet.email(),
-    password: faker.random.alphaNumeric(7),
+    name,
+    email,
+    password,
   };
 };
