bfb916c2fafec5fb059ae5a6eadbeda08b995a1a
vinisaveg <viniisaveeg@gmail.com>
2022-03-23 15:35:10 -0300

    feat: add a Validation to SignUpController

--- src/presentation/controllers/sign-up/sign-up-controller.ts
+++ src/presentation/controllers/sign-up/sign-up-controller.ts
@@ -3,14 +3,25 @@ import { Controller } from "@/presentation/protocols/controller";
 import { HttpResponse } from "@/presentation/protocols/http";
 import { EmailTakenError } from "@/presentation/errors/email-taken-error";
 import { ServerError } from "@/presentation/errors/server-error";
+import { Validation } from "@/validation/protocols/validation";
+import { MissingFieldError } from "@/presentation/errors/missing-field-error";
 
 export class SignUpController
   implements Controller<SignUpParams, SignUpResult>
 {
-  constructor(private readonly remoteSignUp: SignUp) {}
+  constructor(
+    private readonly remoteSignUp: SignUp,
+    private readonly validaton: Validation
+  ) {}
 
   async handle(request: SignUpParams): Promise<HttpResponse<SignUpResult>> {
     try {
+      const error = this.validaton.validate(request);
+
+      if (error) {
+        return { statusCode: 400, body: error };
+      }
+
       const signUpResult = await this.remoteSignUp.execute(request);
 
       if (!signUpResult.result) {
