7c8a6649bebd0ced9c53771499c5b4149bd641d6
vinisaveg <viniisaveeg@gmail.com>
2022-03-23 19:00:26 -0300

    feat: add InvalidEmailValidator

---
+++ src/validation/validators/invalid-email/invalid-email-validator.ts
@@ -0,0 +1,20 @@
+import { InvalidFieldError } from "@/presentation/errors/invalid-field-error";
+import { EmailValidator } from "@/validation/protocols/email-validator";
+import { Validation } from "@/validation/protocols/validation";
+
+export class InvalidEmailValidator implements Validation {
+  constructor(
+    private readonly fieldName: string,
+    private readonly emailValidator: EmailValidator
+  ) {}
+
+  validate<T>(input: Record<string, T>): Error | null {
+    const isValid = this.emailValidator.isValid(input[this.fieldName]);
+
+    if (!isValid) {
+      return new InvalidFieldError("email");
+    }
+
+    return null;
+  }
+}
