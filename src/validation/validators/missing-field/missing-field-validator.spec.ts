17b397b0fdcf720523adbe39b486d29f6c89b784
vinisaveg <viniisaveeg@gmail.com>
2022-03-23 15:15:07 -0300

    refactor: add a makeSut helper function

--- src/validation/validators/missing-field/missing-field-validator.spec.ts
+++ src/validation/validators/missing-field/missing-field-validator.spec.ts
@@ -3,19 +3,24 @@ import { MissingFieldError } from "@/presentation/errors/missing-field-error";
 
 import faker from "@faker-js/faker";
 
+const makeSut = (field: string): MissingFieldValidator => {
+  const sut = new MissingFieldValidator(field);
+  return sut;
+};
+
 describe("Missing Field validator", () => {
   it("Should return a MissingFieldError if validation fails", () => {
     const field = faker.database.column();
-    const sut = new MissingFieldValidator(field);
+    const sut = makeSut(field);
 
-    const error = sut.validate({ invalidFiled: faker.random.word() });
+    const error = sut.validate({ invalidField: faker.random.word() });
 
     expect(error).toEqual(new MissingFieldError(field));
   });
 
   it("Should return null if validation succeeds", () => {
     const field = faker.database.column();
-    const sut = new MissingFieldValidator(field);
+    const sut = makeSut(field);
 
     const error = sut.validate({ [field]: faker.random.word() });
 
