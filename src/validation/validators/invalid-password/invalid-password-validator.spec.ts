b0ab1a39cadcda44274f1da58d375e57d06ba32a
vinisaveg <viniisaveeg@gmail.com>
2022-03-24 14:44:02 -0300

    test: ensure to return an InvalidFieldError if password is invalid

--- src/validation/validators/invalid-password/invalid-password-validator.spec.ts
+++ src/validation/validators/invalid-password/invalid-password-validator.spec.ts
@@ -1,5 +1,6 @@
 import { InvalidPasswordValidator } from "@/validation/validators/invalid-password/invalid-password-validator";
 import { PasswordValidatorSpy } from "./test/password-validator-spy";
+import { InvalidFieldError } from "@/presentation/errors/invalid-field-error";
 
 import faker from "@faker-js/faker";
 
@@ -14,5 +15,17 @@ describe("Invalid Password validator", () => {
     const error = sut.validate({ password: validPassword });
 
     expect(error).toBe(null);
+  });
+
+  it("Should return an InvalidFieldError if password is invalid", () => {
+    const passwordValidatorSpy = new PasswordValidatorSpy();
+    const sut = new InvalidPasswordValidator("password", passwordValidatorSpy);
+    passwordValidatorSpy.result = false;
+
+    const invalidPassword = faker.internet.password();
+
+    const error = sut.validate({ password: invalidPassword });
+
+    expect(error).toEqual(new InvalidFieldError("password"));
   });
 });
