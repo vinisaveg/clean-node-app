4ec4c1be0bac7d27d265c3ceb7345dc60ad4dc9d
vinisaveg <viniisaveeg@gmail.com>
2022-03-24 14:47:33 -0300

    refactor: add a makeSut helper function

--- src/validation/validators/invalid-password/invalid-password-validator.spec.ts
+++ src/validation/validators/invalid-password/invalid-password-validator.spec.ts
@@ -1,30 +1,42 @@
 import { InvalidPasswordValidator } from "@/validation/validators/invalid-password/invalid-password-validator";
-import { PasswordValidatorSpy } from "./test/password-validator-spy";
+import { PasswordValidatorSpy } from "@/validation/validators/invalid-password/test/password-validator-spy";
 import { InvalidFieldError } from "@/presentation/errors/invalid-field-error";
 
 import faker from "@faker-js/faker";
 
+type SutTypes = {
+  passwordValidatorSpy: PasswordValidatorSpy;
+  password: string;
+  sut: InvalidPasswordValidator;
+};
+
+const makeSut = (): SutTypes => {
+  const passwordValidatorSpy = new PasswordValidatorSpy();
+  const sut = new InvalidPasswordValidator("password", passwordValidatorSpy);
+  const password = faker.internet.password();
+
+  return {
+    passwordValidatorSpy,
+    password,
+    sut,
+  };
+};
+
 describe("Invalid Password validator", () => {
   it("Should return null if password is valid", () => {
-    const passwordValidatorSpy = new PasswordValidatorSpy();
-    const sut = new InvalidPasswordValidator("password", passwordValidatorSpy);
+    const { passwordValidatorSpy, password, sut } = makeSut();
     passwordValidatorSpy.result = true;
 
-    const validPassword = faker.internet.password();
-
-    const error = sut.validate({ password: validPassword });
+    const error = sut.validate({ password });
 
     expect(error).toBe(null);
   });
 
   it("Should return an InvalidFieldError if password is invalid", () => {
-    const passwordValidatorSpy = new PasswordValidatorSpy();
-    const sut = new InvalidPasswordValidator("password", passwordValidatorSpy);
+    const { passwordValidatorSpy, password, sut } = makeSut();
     passwordValidatorSpy.result = false;
 
-    const invalidPassword = faker.internet.password();
-
-    const error = sut.validate({ password: invalidPassword });
+    const error = sut.validate({ password });
 
     expect(error).toEqual(new InvalidFieldError("password"));
   });
