6719a32a162d8b7ce1747eddabbab507f6a10073
vinisaveg <viniisaveeg@gmail.com>
2022-03-25 16:09:22 -0300

    test: ensure to return true if password is valid

---
+++ src/infra/validators/invalid-password/invalid-password-validator-adapter.spec.ts
@@ -0,0 +1,20 @@
+import { InvalidPasswordValidatorAdapter } from "@/infra/validators/invalid-password/invalid-password-validator-adapter";
+
+import faker from "@faker-js/faker";
+
+describe("Invalid Password Validator adapter", () => {
+  it("Should return true if password is valid", () => {
+    const sut = new InvalidPasswordValidatorAdapter();
+
+    const strongPassword = faker.internet.password(
+      16,
+      false,
+      /[a-zA-Z0-9!@#$%^&*]/,
+      "aZ0@"
+    );
+
+    const result = sut.isValid(strongPassword);
+
+    expect(result).toBe(true);
+  });
+});
