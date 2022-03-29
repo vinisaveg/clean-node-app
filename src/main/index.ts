2e8c75a7088188993296365d419433b3c0cbb340
vinisaveg <viniisaveeg@gmail.com>
2022-03-29 18:15:10 -0300

    feat: initialize server on dev/start

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world!");
+import "module-alias/register";
+import "dotenv/config";
+
+import { ServerHelper } from "./server/server";
+
+const serverHelper = new ServerHelper(3000);
+
+(async (): Promise<void> => {
+  await serverHelper.init(process.env.MONGODB_URI as string);
+  serverHelper.start();
+})();
