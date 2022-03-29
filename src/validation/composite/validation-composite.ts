2ea286e396493d1265334d2868e623d9d9d374cf
vinisaveg <viniisaveeg@gmail.com>
2022-03-29 09:21:59 -0300

    style: disable eslint no-explicit-any for specific files

--- src/validation/composite/validation-composite.ts
+++ src/validation/composite/validation-composite.ts
@@ -3,6 +3,7 @@ import { Validation } from "@/validation/protocols/validation";
 export class ValidationComposite implements Validation {
   constructor(private readonly validations: Array<Validation>) {}
 
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
   validate(input: Record<string, any>): Error | null {
     for (const validation of this.validations) {
       const error = validation.validate(input);
--- src/validation/protocols/validation.ts
+++ src/validation/protocols/validation.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface Validation {
   validate(input: Record<string, any>): Error | null;
 }
