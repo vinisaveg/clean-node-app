2052625bb5f8fac067f58c51579eea2afd8014f5
vinisaveg <viniisaveeg@gmail.com>
2022-03-30 15:17:10 -0300

    test: ensure to return 400 if request is invalid

--- src/main/routes/authentication/authentication-routes.test.ts
+++ src/main/routes/authentication/authentication-routes.test.ts
@@ -1,5 +1,6 @@
 import { MongoHelper } from "@/infra/database/mongodb/mongo-helper";
 import { ServerHelper } from "@/main/server/server";
+import faker from "@faker-js/faker";
 
 import { Application } from "express";
 import { Collection } from "mongodb";
@@ -8,6 +9,12 @@ import request from "supertest";
 describe("Authentication routes", () => {
   let usersCollection: Collection;
   let app: Application;
+  const strongPassword = faker.internet.password(
+    16,
+    false,
+    /[a-zA-Z0-9!@#$%^&*]/,
+    "aZ0@"
+  );
 
   beforeAll(async () => {
     const serverHelper = new ServerHelper(3000);
@@ -27,13 +34,22 @@ describe("Authentication routes", () => {
       const response = await request(app).post("/api/signup").send({
         name: "vinicius",
         email: "vinicius@test.com",
-        password: "abC12&*c5F@0",
+        password: strongPassword,
       });
 
       expect(response.status).toBe(201);
       expect(response.body).toHaveProperty("result", true);
       expect(response.body).toHaveProperty("name", "vinicius");
       expect(response.body).toHaveProperty("accessToken");
+    });
+
+    it("Should return 400 if the request is invalid", async () => {
+      const response = await request(app).post("/api/signup").send({
+        name: faker.name.findName(),
+        password: strongPassword,
+      });
+
+      expect(response.status).toBe(400);
     });
   });
 });
