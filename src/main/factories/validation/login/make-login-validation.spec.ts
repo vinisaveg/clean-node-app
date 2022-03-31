c530cf021e0c3f62c3125d03c6981c0e802e33e6
vinisaveg <viniisaveeg@gmail.com>
2022-03-31 18:22:07 -0300

    test: ensure to return a Validation with correct validators

---
+++ src/main/factories/validation/login/make-login-validation.spec.ts
@@ -0,0 +1,34 @@
+import { InvalidEmailValidatorAdapter } from "@/infra/validators/invalid-email/invalid-email-validator-adapter";
+import { InvalidPasswordValidatorAdapter } from "@/infra/validators/invalid-password/invalid-password-validator-adapter";
+import { ValidationComposite } from "@/validation/composite/validation-composite";
+import { InvalidEmailValidator } from "@/validation/validators/invalid-email/invalid-email-validator";
+import { InvalidPasswordValidator } from "@/validation/validators/invalid-password/invalid-password-validator";
+import { MissingFieldValidator } from "@/validation/validators/missing-field/missing-field-validator";
+import { makeLoginValidation } from "@/main/factories/validation/login/make-login-validation";
+
+describe("Login Validation factory", () => {
+  it("Should return a Validation with correct validators", () => {
+    const validators = [];
+
+    for (const field in ["email", "password"]) {
+      validators.push(new MissingFieldValidator(field));
+    }
+
+    const invalidEmailValidatorAdapter = new InvalidEmailValidatorAdapter();
+    validators.push(
+      new InvalidEmailValidator("email", invalidEmailValidatorAdapter)
+    );
+
+    const invalidPasswordValidatorAdapter =
+      new InvalidPasswordValidatorAdapter();
+    validators.push(
+      new InvalidPasswordValidator("password", invalidPasswordValidatorAdapter)
+    );
+
+    const loginValidation = new ValidationComposite(validators);
+
+    const sut = makeLoginValidation();
+
+    expect(sut).toEqual(loginValidation);
+  });
+});
