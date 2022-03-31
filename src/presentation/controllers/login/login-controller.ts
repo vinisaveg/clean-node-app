d8043c28a06a7560302a1b607b83c5c8f50f05d1
vinisaveg <viniisaveeg@gmail.com>
2022-03-31 17:36:09 -0300

    feat: add a Validation to LoginController

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,13 +1,23 @@
 import { Login, LoginParams, LoginResult } from "@/domain/use-cases/login";
-import { forbidden, ok } from "@/presentation/helpers/http-helper";
+import { badRequest, forbidden, ok } from "@/presentation/helpers/http-helper";
 import { Controller } from "@/presentation/protocols/controller";
 import { HttpResponse } from "@/presentation/protocols/http";
 import { InvalidCredentialsError } from "@/presentation/errors/invalid-credentials-error";
+import { Validation } from "@/validation/protocols/validation";
 
 export class LoginController implements Controller<LoginParams, LoginResult> {
-  constructor(private readonly remoteLogin: Login) {}
+  constructor(
+    private readonly remoteLogin: Login,
+    private readonly validation: Validation
+  ) {}
 
   async handle(request: LoginParams): Promise<HttpResponse<LoginResult>> {
+    const error = this.validation.validate(request);
+
+    if (error) {
+      return badRequest(error);
+    }
+
     const loginResult = await this.remoteLogin.execute(request);
 
     if (loginResult.result) {
