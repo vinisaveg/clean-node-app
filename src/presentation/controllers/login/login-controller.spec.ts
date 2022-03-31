e34b3a0972a858f9a4cecdfc3c0a67c157bd2134
vinisaveg <viniisaveeg@gmail.com>
2022-03-31 17:37:09 -0300

    test: ensure to return 400 if request is invalid

--- src/presentation/controllers/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login-controller.spec.ts
@@ -1,18 +1,25 @@
 import { mockLoginParams } from "@/../test/mocks/login/mock-login";
 import { RemoteLoginSpy } from "@/presentation/controllers/login/test/remote-login-spy";
 import { LoginController } from "@/presentation/controllers/login/login-controller";
+import { ValidationSpy } from "@/../test/spies/authentication/validation-spy";
+import { MissingFieldError } from "@/presentation/errors/missing-field-error";
+
+import faker from "@faker-js/faker";
 
 type SutTypes = {
   remoteLoginSpy: RemoteLoginSpy;
+  validationSpy: ValidationSpy;
   sut: LoginController;
 };
 
 const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy();
   const remoteLoginSpy = new RemoteLoginSpy();
-  const sut = new LoginController(remoteLoginSpy);
+  const sut = new LoginController(remoteLoginSpy, validationSpy);
 
   return {
     remoteLoginSpy,
+    validationSpy,
     sut,
   };
 };
@@ -41,5 +48,24 @@ describe("Login controller", () => {
     expect(httpResponse.body).toHaveProperty("result", true);
     expect(httpResponse.body).toHaveProperty("name");
     expect(httpResponse.body).toHaveProperty("accessToken");
+  });
+
+  it("Should return 400 if request is invalid", async () => {
+    const { validationSpy, remoteLoginSpy, sut } = makeSut();
+
+    const request = mockLoginParams();
+
+    remoteLoginSpy.result = true;
+    validationSpy.error = new MissingFieldError(faker.database.column());
+
+    const httpResponse = await sut.handle(request);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual({
+      error: {
+        name: validationSpy.error.name,
+        message: validationSpy.error.message,
+      },
+    });
   });
 });
