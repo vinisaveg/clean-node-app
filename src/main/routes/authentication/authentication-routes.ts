52878f7298ef4ef5bd5045232df5b2284520a307
vinisaveg <viniisaveeg@gmail.com>
2022-04-01 08:37:47 -0300

    feat: add /login route

--- src/main/routes/authentication/authentication-routes.ts
+++ src/main/routes/authentication/authentication-routes.ts
@@ -1,8 +1,10 @@
 import { adaptRoute } from "@/main/adapters/express/adapt-route";
 import { makeSignUpController } from "@/main/factories/controllers/sign-up/make-sign-up-controller";
+import { makeLoginController } from "@/main/factories/controllers/login/make-login-controller";
 
 import { Router } from "express";
 
 export const authenticationRoutes = (router: Router): void => {
   router.post("/signup", adaptRoute(makeSignUpController()));
+  router.post("/login", adaptRoute(makeLoginController()));
 };
